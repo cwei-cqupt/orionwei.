3ba47641ee6ba1e6a682c5a250a39a495306935f
Udf <undefined.weo@gmail.com>
2016-01-19 14:53:26 +0800

    test

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by w on 2016/1/7.
+ */
+pageInit();
+window.onresize = function(){
+    console.log(1);
+    canvasInit();
+};
+function pageInit(){
+    canvasInit();
+}
+var cav;
+function canvasInit(){
+    cav = document.getElementById("main");
+    cav.height = document.documentElement.clientHeight;
+    cav.width = document.documentElement.clientWidth;
+}
